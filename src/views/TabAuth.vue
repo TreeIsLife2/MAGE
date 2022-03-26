deed3d1aa85573db64a86f6a332f5376b1967642
TreeIsLife2 <71183248+TreeIsLife2@users.noreply.github.com>
2022-03-26 22:32:45 +0100

    Update TabAuth.vue

--- src/views/TabAuth.vue
+++ src/views/TabAuth.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="auth" class="h-full grid pl-10 pr-10 grid-cols-2 grid-rows-1">
+  <div id="auth" class="h-full grid pl-10 pr-10 grid-cols-2 grid-rows-1 dark:bg-gray-900">
     <div id="accounts-wrapper" class="col-start-1 row-start-1">
       <div class="account-container" v-for="(farm, index) in farms" :key="index">
-        <div class="account p-4 bg-gray-200 mt-1 mb-1 hover:bg-gray-300 select-none flex" v-if="!farm.isFarm">
+        <div class="account p-4 bg-gray-200 dark:bg-gray-800 mt-1 mb-1 hover:bg-gray-300 dark:hover:bg-gray-900 select-none flex" v-if="!farm.isFarm">
           <div>{{farm.name}}</div>
           <div class="ml-auto">
             <button class="btn">Edit</button>
@@ -18,7 +18,7 @@
     <div id="edits-wrapper" class="hidden col-start-2 col-end-3 row-start-1">
     </div>
     <div id="right-col-wrapper" class="col-start-2 col-end-3 row-start-1"><svg-icon icon="magelogo" noinvert="true" /></div>
-    <div id="profiles-wrapper" class="hidden col-start-1 col-end-3 row-start-1 bg-white">
+    <div id="profiles-wrapper" class="hidden col-start-1 col-end-3 row-start-1 bg-white dark:bg-gray-900">
       <cp />
       <button class="btn" @click="cancelProfileForm">Cancel</button>
     </div>
@@ -61,7 +61,4 @@ export default {
 </script>
 
 <style>
-  .btn {
-    @apply bg-gray-400 text-gray-100 p-1 rounded-md ml-1 hover:bg-gray-600;
-  }
 </style>
